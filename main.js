87d32e177a0b0dbb7c7b1c0200e2f734fe60a30e
Ricardo <halfonso09@gmail.com>
2018-07-21 11:16:09 -0500

    New function ab

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 'use strict';
 
 function ab() {
-<<<<<<< HEAD
-    console.log('This is a change form master');
-=======
     console.log('Git Introduction - This a feature branch "feature_ab"');
->>>>>>> Adding feature branch
 }
